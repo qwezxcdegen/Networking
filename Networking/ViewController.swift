6680fcf1ca568250b96ce53131567dd1fdd3027a
qwezxcdegen <ifomstep@icloud.com>
2023-04-10 18:30:20 +0500

    .

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -11,9 +11,51 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func getRequest() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            return
+        }
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            guard let data, let response, error == nil else { return }
+            print(response)
+        }.resume()
+    }
+    
+    @IBAction func postRequest() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            return
+        }
+        
+        let userData = ["Course": "Networking", "Lesson": "GETPOST"]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData) else {
+            return
+        }
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { data, response, error in
+            guard let data, let response, error == nil else {
+                return
+            }
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data)
+                print(json)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+    }
 }
 
